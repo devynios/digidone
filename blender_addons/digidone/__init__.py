d3aae4fc0d0b6a3fe5c353eb8194668b1f3a0391
Ignas Bačius <ignas.bacius@gmail.com>
2015-08-15 14:15:30 +0300

    Add "edit", "remove" parameter operators

--- blender_addons/digidone/__init__.py
+++ blender_addons/digidone/__init__.py
@@ -13,25 +13,22 @@ bl_info = {
 import bpy
 
 
-digidone_type_items = [
-    ("FLOAT"  , "Float"  , '', 0),
-    ("INTEGER", "Integer", '', 1),
-    ("BOOLEAN", "Boolean", '', 2),
-    ("STRING" , "String" , '', 3),
-]
-
-
-digidone_modes = [
-    ("OBJET", "Object", '', 0),
-    ("EDIT" , "Edit"  , '', 1),
+digidone_param_type_items = [
+    ('FLOAT'  , 'Float'  , '', 0),
+    ('INTEGER', 'Integer', '', 1),
+    ('BOOLEAN', 'Boolean', '', 2),
+    ('STRING' , 'String' , '', 3),
 ]
 
 
 class DigidoneParameter(bpy.types.PropertyGroup):
-    ptype =  bpy.props.EnumProperty(name='Parameter Type', items=digidone_type_items)
-    name = bpy.props.StringProperty(name="Parameter Name")
-    group = bpy.props.StringProperty(name="Parameter Group")
-    value = bpy.props.FloatProperty(name="Value Parameter")
+    ptype =  bpy.props.EnumProperty(name='Parameter Type', items=digidone_param_type_items)
+    name = bpy.props.StringProperty(name='Parameter Name')
+    group = bpy.props.StringProperty(name='Parameter Group')
+    value_FLOAT = bpy.props.FloatProperty(name='Parameter Value')
+    value_INTEGER = bpy.props.IntProperty(name='Parameter Value')
+    value_BOOLEAN = bpy.props.BoolProperty(name='Parameter Value')
+    value_STRING = bpy.props.StringProperty(name='Parameter Value')
 
 
 class OBJECT_OT_digidone_component_create(bpy.types.Operator):
@@ -54,7 +51,7 @@ class OBJECT_OT_digidone_component_create(bpy.types.Operator):
             obj.select = True
         actobj.select = False
         actobj.select = True
-        bpy.ops.object.parent_set(type="OBJECT")
+        bpy.ops.object.parent_set(type='OBJECT')
         return {'FINISHED'}
 
 
@@ -65,8 +62,55 @@ class OBJECT_OT_digidone_component_addparam(bpy.types.Operator):
     def execute(self, context):
         obj = bpy.context.active_object
         param = obj.dgd_params.add()
-        # TODO
         return {'FINISHED'}
+
+
+class OBJECT_OT_digidone_component_delparam(bpy.types.Operator):
+    bl_idname = "object.digidone_component_delparam"
+    bl_label = "Remove Parameter"
+
+    index = bpy.props.IntProperty(name='Index', default=-1, options={'HIDDEN'})
+
+    def execute(self, context):
+        idx = self.index
+        if idx < 0:
+            return {'CANCELLED'}
+        obj = bpy.context.active_object
+        obj.dgd_params.remove(idx)
+        return {'FINISHED'}
+
+
+class OBJECT_OT_digidone_component_editparam(bpy.types.Operator):
+    bl_idname = "object.digidone_component_editparam"
+    bl_label = "Edit Parameter"
+
+    index = bpy.props.IntProperty(name='Index', default=-1, options={'HIDDEN'})
+    name = bpy.props.StringProperty(name='Parameter Name')
+    ptype =  bpy.props.EnumProperty(name='Parameter Type', items=digidone_param_type_items)
+    group = bpy.props.StringProperty(name='Parameter Group')
+
+    def execute(self, context):
+        idx = self.index
+        if idx < 0:
+            return {'CANCELLED'}
+        obj = bpy.context.active_object
+        param = obj.dgd_params[idx]
+        param.name = self.name
+        param.ptype = self.ptype
+        param.group = self.group
+        for win in context.window_manager.windows:
+            for area in win.screen.areas:
+                area.tag_redraw()
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        idx = self.index
+        obj = bpy.context.active_object
+        param = obj.dgd_params[idx]
+        self.name = param.name
+        self.ptype = param.ptype
+        self.group = param.group
+        return context.window_manager.invoke_props_dialog(self)
 
 
 class OBJECT_PT_digidone_parameters(bpy.types.Panel):
@@ -87,9 +131,10 @@ class OBJECT_PT_digidone_parameters(bpy.types.Panel):
             return
         layout.prop(actobj, 'dgd_object_type')
         for param in actobj.dgd_params:
-            row = layout.row()
-            row.label(text=param['name'])
-            row.column().prop(param, 'value', text='Value')
+            pname = param.get('name')
+            if not pname:
+                continue
+            layout.prop(param, 'value_%s' % (param.ptype,), text=pname)
 
 
 class OBJECT_PT_digidone_edit_parameters(bpy.types.Panel):
@@ -112,25 +157,36 @@ class OBJECT_PT_digidone_edit_parameters(bpy.types.Panel):
             return
         layout.prop(actobj, 'name')
         layout.operator('object.digidone_component_addparam')
-        for param in actobj.dgd_params:
+        for i, param in enumerate(actobj.dgd_params):
             row = layout.row()
-            row.column().prop(param, 'ptype', text='Type')
-            row.column().prop(param, 'name', text='Name')
-            row.column().prop(param, 'group', text='Group')
+            row.column().prop(param, 'name', text='')
+            row = row.column().row(align=True)
+            op = row.operator('object.digidone_component_editparam', text='Edit')
+            op.index = i
+            op = row.operator('object.digidone_component_delparam', text='', icon='ZOOMOUT')
+            op.index = i
+            op = None
 
 
-def digidone_type_items(self, context):
+def digidone_obj_type_items(self, context):
     objlist = []
     for obj in context.scene.objects:
         if obj.get('dgd_is_parametric'):
-            objlist.append(tuple([param.value for param in obj.dgd_params]))
+            objlist.append(tuple([getattr(param, 'value_%s' % (param.ptype,)) for param in obj.dgd_params]))
     return [(str(i), 'Type %d' % (i,), '', i) for i, obj in enumerate(set(objlist))]
+
+
+digidone_modes = [
+    ('OBJECT', 'Object', '', 0),
+    ('EDIT'  , 'Edit'  , '', 1),
+]
 
 
 def register():
     bpy.utils.register_module(__name__)
+    bpy.types.Object.dgd_is_parametric = bpy.props.BoolProperty(name='Is Parametric')
     bpy.types.Object.dgd_params = bpy.props.CollectionProperty(type=DigidoneParameter)
-    bpy.types.Object.dgd_object_type = bpy.props.EnumProperty(name='Type', items=digidone_type_items)
+    bpy.types.Object.dgd_object_type = bpy.props.EnumProperty(name='Type', items=digidone_obj_type_items)
     bpy.types.Object.dgd_mode = bpy.props.EnumProperty(name='Mode', items=digidone_modes)
 
 
